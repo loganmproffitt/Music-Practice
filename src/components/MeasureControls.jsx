275583ef09f0e75c3fa1d1d4d11963f32efab632
Logan Proffitt <lopr6007@colorado.edu>
2025-02-03 12:32:37 -0700

    UI added for measures on and off. Changing both during skipping works.

--- src/components/MeasureControls.jsx
+++ src/components/MeasureControls.jsx
@@ -7,7 +7,7 @@ function MeasureControls() {
 
     return (
       <div>
-        <h2>Measure Settings</h2>
+        <h2>Settings</h2>
 
         {/* Time signature settings */}
         <label>Time Signature:</label>
@@ -41,7 +41,7 @@ function MeasureControls() {
         
         <div>
           {/* Skip Measures Selection */}
-          <label>
+          <label>Skip Measures:
             <input
               type="checkbox"
               checked={measureSettings.skipping.skippingEnabled}
@@ -53,9 +53,57 @@ function MeasureControls() {
                 }
               }))}
             />
-          Skip Measures:</label>
+          </label>
         </div>
-     
+
+        <div>
+          {/* Measures On Selection */}
+          <label>Measures On:
+          <select
+            value={measureSettings.skipping.measuresOn}
+            onChange={(e) =>
+            setMeasureSettings((prev) => ({
+              ...prev,
+              skipping: {
+                ...prev.skipping,
+                measuresOn: Number(e.target.value),
+              }
+            }))
+          }>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          </select>
+          </label>
+        </div>
+
+        <div>
+          {/* Measures Off Selection */}
+          <label>Measures Off:
+            <select
+              value={measureSettings.skipping.measuresOff}
+              onChange={(e) =>
+              setMeasureSettings((prev) => ({
+                ...prev,
+                skipping: {
+                  ...prev.skipping,
+                  measuresOff: Number(e.target.value),
+                }
+              }))
+            }>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            </select>
+          </label>
+        </div>
+      
       </div>
       
     );
